c19aa6df414936e058b23da53398268acc1edafa
zzlnewair <zzl2007315@163.com>
2016-06-01 22:31:08 +0800

    静态广播和动态广播

--- 4Broadcast/01BcastRecCompBcast/src/com/zzl/broadcastreceiver/compoundbroadcast/CompoundBroadcast.java
+++ 4Broadcast/01BcastRecCompBcast/src/com/zzl/broadcastreceiver/compoundbroadcast/CompoundBroadcast.java
@@ -11,6 +11,7 @@ import android.widget.Button;
 public class CompoundBroadcast extends Activity {
 
 	private static final String CUSTOM_INTENT = "com.zzl.BroadcastReceiver.show_toast";
+	private static final String CUSTOM_INTENT2 = "com.zzl.BroadcastReceiver.show_toast2";
 	private final Receiver1 receiver1 = new Receiver1();
 	private final IntentFilter intentFilter = new IntentFilter(CUSTOM_INTENT);
 
@@ -30,6 +31,14 @@ public class CompoundBroadcast extends Activity {
 			}
 		});
 
+		Button button2 = (Button) findViewById(R.id.button2);
+		button2.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				sendBroadcast(new Intent(CUSTOM_INTENT2));
+			}
+		});
+		
 	}
 
 	@Override
