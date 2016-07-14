82f88cde4b4af451324024f9bcdfafd180343269
zzlnewair <zzl2007315@163.com>
2016-07-14 21:41:34 +0800

    WebServer

---
+++ 7network/webServer/src/com/zzl/service/impl/VideoNewsServiceBean.java
@@ -0,0 +1,18 @@
+package com.zzl.service.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.zzl.domain.News;
+import com.zzl.service.VideoNewsService;
+
+public class VideoNewsServiceBean implements VideoNewsService {
+
+	public List<News> getLastNews(){
+		List<News> newes = new ArrayList<News>();
+		newes.add(new News(90, "Ï²ÑòÑòÓë»ÒÌ«ÀÇÈ«¼¯", 78));
+		newes.add(new News(10, "ÊµÅÄ½¢ÔØÖ±Éı¶«º£¾ÈÔ®ÑİÏ°", 28));
+		newes.add(new News(56, "¿¦ÂóÂ¡VSºÉÀ¼", 70));
+		return newes;
+	}
+}
