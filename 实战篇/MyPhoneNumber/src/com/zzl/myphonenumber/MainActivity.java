9894b62c4f0e654ab2a8d4e214b732d7992b07ff
zzlnewair <zzl2007315@163.com>
2016-08-23 12:55:38 +0800

    Volley

---
+++ 实战篇/MyPhoneNumber/src/com/zzl/myphonenumber/MainActivity.java
@@ -0,0 +1,24 @@
+package com.zzl.myphonenumber;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ListView;
+
+
+public class MainActivity extends Activity {
+	
+	private ListView lv;
+	private MyAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        GetNumber.getNumber(this);
+        lv = (ListView) findViewById(R.id.lv);
+        adapter = new MyAdapter(GetNumber.lists, this);
+        lv.setAdapter(adapter);
+    }
+}
---
+++ 实战篇/MyPhoneNumber/src/com/zzl/myphonenumber/PhoneInfo.java
@@ -0,0 +1,27 @@
+package com.zzl.myphonenumber;
+
+public class PhoneInfo {
+	private String name;
+	private String number;
+	
+	public PhoneInfo(String name,String number) {
+		setNumber(number);
+		setName(name);
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getNumber() {
+		return number;
+	}
+	public void setNumber(String number) {
+		this.number = number;
+	}
+	
+	
+
+}
